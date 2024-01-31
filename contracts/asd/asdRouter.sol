7a15f748c3ea04b27192bdf0d29caffaa9d8e579
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2024-01-31 18:23:06 -0500

    add full router functionality support for minting ASD and sending cross chain

--- contracts/asd/asdRouter.sol
+++ contracts/asd/asdRouter.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.22;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IOAppComposer} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/interfaces/IOAppComposer.sol";
 import {OFTComposeMsgCodec} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/libs/OFTComposeMsgCodec.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ASDOFT} from "./asdOFT.sol";
+import {IOFT, SendParam, MessagingFee} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
+import {OptionsBuilder} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/libs/OptionsBuilder.sol";
 
 /**
  * @title ASDRouter
@@ -10,10 +14,24 @@ import {OFTComposeMsgCodec} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/li
 
 contract ASDRouter is IOAppComposer, Ownable {
     address public noteAddress;
-    uint16 public cantoLzEID;
+    uint32 public cantoLzEID;
     mapping(address => bool) public whitelistedOFTStableCoins;
 
-    constructor(address _noteAddress, uint16 _cantoLzEID) {
+    struct OftComposeMessage {
+        uint32 _dstLzEid;
+        address _dstReceiver;
+        address _dstAsdAddress;
+        address _cantoAsdAddress;
+        uint256 _minAmountASD;
+        address _cantoRefundAddress;
+        uint256 _feeForSend;
+    }
+
+    event LZReceived(address _from, bytes32 _guid, bytes _message, address _executor, bytes _extraData, uint _value);
+
+    event TokenRefund(address _tokenAddress, address _refundAddress, uint _amount, uint _nativeAmount, string _reason);
+
+    constructor(address _noteAddress, uint32 _cantoLzEID) {
         noteAddress = _noteAddress;
         cantoLzEID = _cantoLzEID;
     }
@@ -28,20 +46,153 @@ contract ASDRouter is IOAppComposer, Ownable {
         whitelistedOFTStableCoins[_oftAddress] = _whitelisted;
     }
 
-    event LZReceived(address _from, bytes32 _guid, bytes _message, address _executor, bytes _extraData, uint _value);
-    event RouterPayload(uint64 _nonce, uint32 _srcEid, uint256 _amountLD, bytes32 _composeFrom, bytes _composeMsg);
-
+    /**
+     * @notice Called by the LZ executor after sending IFT tokens to this contract.
+     * @param _from The address of the OFT on this chain.
+     * @param _guid The GUID of the message.
+     * @param _message The message payload from the executor formatted as an OFT composed message.
+     * @param _executor The address of the executor.
+     * @param _extraData Additional data supplied by the executor.
+     * @dev Cannot revert anywhere, must send the tokens to the intended receiver if something fails (token's will be lost otherwise)
+     */
     function lzCompose(address _from, bytes32 _guid, bytes calldata _message, address _executor, bytes calldata _extraData) external payable {
+        /* log event */
         emit LZReceived(_from, _guid, _message, _executor, _extraData, msg.value);
-        (uint64 nonce, uint32 srcEid, uint256 amountLD, bytes32 composeFrom, bytes memory composeMsg) = _decodeOFTComposeMsg(_message);
-        emit RouterPayload(nonce, srcEid, amountLD, composeFrom, composeMsg);
+
+        /* decode OFT composed message */
+        (, , uint256 amountLD, bytes32 composeFrom, bytes memory composeMsg) = _decodeOFTComposeMsg(_message);
+
+        /* decode composed message payload */
+
+        // check the composed message for proper formatting
+        if (composeMsg.length != 224) {
+            // return tokens to the address that sent them (composeFrom)
+            _refundToken(_from, OFTComposeMsgCodec.bytes32ToAddress(composeFrom), amountLD, msg.value, "Invalid composeMsg length");
+            return;
+        }
+        OftComposeMessage memory payload = abi.decode(composeMsg, (OftComposeMessage));
+
+        /* check if the OFT stable coin is whitelisted */
+        if (!whitelistedOFTStableCoins[_from]) {
+            // return tokens to the refund address on canto
+            _refundToken(_from, payload._cantoRefundAddress, amountLD, msg.value, "not whitelisted");
+            return;
+        }
+
+        /* swap tokens for $NOTE (check minAmount for slippage) */
+        (uint amountNote, bool successfulSwap) = _swapOFTForNote(_from, amountLD, payload._minAmountASD);
+
+        // check if the swap was successful
+        if (!successfulSwap) {
+            // return tokens to the refund address on canto
+            _refundToken(_from, payload._cantoRefundAddress, amountLD, msg.value, "swap failed");
+            return;
+        }
+
+        /* deposit $NOTE to the correct asd vault to receive ASD tokens */
+        (bool successfulDeposit, string memory reason) = _depositNoteToASDVault(payload._cantoAsdAddress, amountNote);
+
+        // check if deposit was successful
+        if (!successfulDeposit) {
+            // return $NOTE to the refund address on canto since OFT was swapped already
+            _refundToken(noteAddress, payload._cantoRefundAddress, amountNote, msg.value, reason);
+            return;
+        }
+
+        /* transfer the ASD tokens to the destination receiver */
+        if (payload._dstLzEid == cantoLzEID) {
+            // just transfer the ASD tokens to the destination receiver
+            ASDOFT(payload._cantoAsdAddress).transfer(payload._dstReceiver, amountNote);
+        } else {
+            // use Layer Zero oapp to send ASD tokens to the destination receiver on the destination chain
+
+            // make sure msg.value is enough to cover the fee or this transaction will revert
+            if (msg.value < payload._feeForSend) {
+                // refund ASD tokens on canto
+                _refundToken(payload._cantoAsdAddress, payload._cantoRefundAddress, amountNote, msg.value, "insufficient msg.value for send fee");
+                return;
+            }
+
+            // create send params for the Layer Zero oapp
+            bytes memory sendOptions = OptionsBuilder.addExecutorLzReceiveOption(OptionsBuilder.newOptions(), 200000, 0);
+            SendParam memory sendParams = SendParam({dstEid: payload._dstLzEid, to: OFTComposeMsgCodec.addressToBytes32(payload._dstReceiver), amountLD: amountNote, minAmountLD: amountNote, extraOptions: sendOptions, composeMsg: "0x", oftCmd: "0x"});
+            MessagingFee memory fee = MessagingFee({nativeFee: payload._feeForSend, lzTokenFee: 0});
+
+            // send tokens
+            (bool successfulSend, bytes memory data) = payable(payload._cantoAsdAddress).call{value: payload._feeForSend}(abi.encodeWithSelector(IOFT.send.selector, sendParams, fee, payload._cantoRefundAddress));
+
+            // check if the send was successful
+            if (!successfulSend) {
+                // refund ASD tokens on canto
+                _refundToken(payload._cantoAsdAddress, payload._cantoRefundAddress, amountNote, msg.value, string(data));
+                return;
+            }
+        }
     }
 
+    /**
+     *
+     * @param _message The message payload from the executor formatted as an OFT composed message.
+     * @return _nonce The nonce value.
+     * @return _srcEid The source endpoint ID.
+     * @return _amountLD The amount in local decimals.
+     * @return _composeFrom The composeFrom value (msg.sender on from chain).
+     * @return _composeMsg The composed message.
+     */
     function _decodeOFTComposeMsg(bytes calldata _message) internal pure returns (uint64 _nonce, uint32 _srcEid, uint256 _amountLD, bytes32 _composeFrom, bytes memory _composeMsg) {
         _nonce = OFTComposeMsgCodec.nonce(_message);
         _srcEid = OFTComposeMsgCodec.srcEid(_message);
         _amountLD = OFTComposeMsgCodec.amountLD(_message);
         _composeFrom = OFTComposeMsgCodec.composeFrom(_message);
         _composeMsg = OFTComposeMsgCodec.composeMsg(_message);
+    }
+
+    /**
+     * @notice refunds tokens when lzCompose fails
+     * @param _tokenAddress address of token
+     * @param _refundAddress address to refund to on canto
+     * @param _amount amount to send
+     * @param _nativeAmount amount to send in native token
+     */
+    function _refundToken(address _tokenAddress, address _refundAddress, uint _amount, uint _nativeAmount, string memory _reason) internal {
+        // emit event
+        emit TokenRefund(_tokenAddress, _refundAddress, _amount, _nativeAmount, _reason);
+        // transfer tokens to refund address
+        IERC20(_tokenAddress).transfer(_refundAddress, _amount);
+        // transfer native tokens to refund address and check that this value is less than or equal to msg.value
+        if (_nativeAmount > 0 && _nativeAmount <= msg.value) {
+            payable(_refundAddress).transfer(_nativeAmount);
+        }
+    }
+
+    /**
+     * @notice deposits $NOTE to the correct asd vault to receive ASD tokens
+     * @param _asdVault The address of the ASD vault to deposit to
+     * @param _amountNote The amount of $NOTE to deposit
+     */
+    function _depositNoteToASDVault(address _asdVault, uint _amountNote) internal returns (bool, string memory) {
+        // approve asd vault to spend $NOTE
+        IERC20(noteAddress).approve(_asdVault, _amountNote);
+        // deposit $NOTE to asd vault (use call, so this doesn't revert)
+        (bool success, bytes memory errReason) = _asdVault.call(abi.encodeWithSelector(ASDOFT.mint.selector, _amountNote));
+        return (success, string(errReason));
+    }
+
+    /**
+     * @notice swaps an OFT stable coin for $NOTE
+     * @dev only callable by the owner
+     * @param _oftAddress The address of the OFT stable coin contract
+     * @param _amount The amount of OFT stable coin to swap
+     * @param _minAmountNote The minimum amount of $NOTE to receive
+     * @return amount The amount of $NOTE received or error code
+     */
+    function _swapOFTForNote(address _oftAddress, uint _amount, uint _minAmountNote) internal returns (uint amount, bool success) {
+        if (_amount < _minAmountNote) {
+            amount = 0;
+            success = false;
+        } else {
+            amount = _amount;
+            success = true;
+        }
     }
 }
